4618d4aa279be79050b710cf93ff8a10f2b90854
LAPTOP-LKM2025\rhksa <rhksals7864@gmail.com>
2025-03-26 16:08:58 +0900

    동전교환 지폐 단위 추가

--- assignment03.py
+++ assignment03.py
@@ -1,14 +1,16 @@
 # 동전 교환 프로그램
+# total = int(input("교환할 돈은 얼마인가?  "))
+total = 777777
 
-total = int(input("교환할 돈은 얼마인가?  "))
-
-coins = [500, 100, 50, 10]
-number_coins = [ 0, 0, 0, 0 ]
+coins = [50000, 10000, 5000, 1000, 500, 100, 50, 10]
+number_coins = []
 change = total
 
 for i, coin in enumerate(coins):
-    number_coins[i] = change // coin  
+    number_coins.append(change // coin)  
     change = change % coin
-    print(coin, number_coins[i])
+    # print(coin, number_coins[i])
 
-print("500원 짜리 %d개" % (number_coins[0]))
+for i in range(coins.__len__()):
+    print("%d원짜리 %d개" % (coins[i], number_coins[i]))
+print("바꾸지 못한 잔돈 %d" % change)
