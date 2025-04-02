92d343e50271719cad8f920278c49fb553849a95
LAPTOP-LKM2025\rhksa <rhksals7864@gmail.com>
2025-04-02 16:12:12 +0900

    학점 계산

--- assignment05.py
+++ assignment05.py
@@ -1,18 +1,13 @@
-import turtle
-
-
-
 a = int(input('정수 입력 : '))
 
-leap_year = False
-
-if a % 4 == 0:
-    if a % 100 != 0: 
-        leap_year = True
-    elif a % 400 == 0:
-        leap_year = True
-
-if leap_year:
-    print('윤년')
+if( a >= 90 ):
+    print('A', end='')
+elif a >= 80:
+    print('B', end='')
+elif a>= 70:
+    print('C', end='')
+elif a >= 60:
+    print('D', end='')
 else:
-    print('평년')
+    print('F', end='')
+print("학점 ^^")
