cf162fff20d9241aed3b24ecff79cac6d9136f10
LAPTOP-LKM2025\rhksa <rhksals7864@gmail.com>
2025-04-02 17:12:11 +0900

    계산기 실습

--- assignment05.py
+++ assignment05.py
@@ -1,19 +1,12 @@
-import random
+menu = input("입력한 수식 계산 : 1 | 두 수 사이의 합계 : 2  ")
 
-cnts = 0
-times = 100000
-
-for i in range(times):
-    cnt = 0
-    numbers = []
-
-    for num in range(0, 100):
-        numbers.append(random.randrange(0, 100))
-
-    for num in range(0, 100):
-        if num not in numbers:
-            cnt+=1
-    cnts += cnt
-
-print(cnts / times)
-        
+if menu == '1':
+    numstr = input(" 수식을 입력하세요 : ")
+    print('결과는 %5.1f입니다' % eval(numstr))
+elif menu == '2':
+    num1 = int(input('첫번째 숫자를 입력 : '))
+    num2 = int(input('두번째 숫자를 입력 : '))
+    result = ((num2 - num1 + 1) * (num2 + num1) / 2)
+    print('결과는 %d입니다.' % result)
+else:
+    print('잘못된 요청입니다.')
