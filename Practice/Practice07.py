3891862c682db34a11bd4414dbd9d0cec8a534f1
LAPTOP-LKM2025\rhksa <rhksals7864@gmail.com>
2025-04-16 17:20:49 +0900

    7주차 실습

---
+++ Practice/Practice07.py
@@ -0,0 +1,15 @@
+# 7주차 실습 시간 예제 (list)
+
+import turtle
+import random
+
+myTurtle, tx, ty, tColor, tSize, tShape = [None] * 6
+shapeList = turtle.getshapes()
+
+swidth, sheight = 500, 500
+
+for i in range(100):
+    random.shuffle(shapeList)
+    myTurtle = turtle.Turtle(shapeList[0])
+    tx = random.randint()
+
